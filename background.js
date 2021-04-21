7c4255761af8cd520a8205294b68b341f10d1268
Vishnu Gopy <vishnugoby0010@gmail.com>
2021-04-21 20:57:59 +0200

    changed some codes

--- background.js
+++ background.js
@@ -1,27 +1,16 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
+
 
   chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
+    chrome.storage.sync.set({grey: '#404040' , white: '#ffffff'}, function() {
       console.log('The color is green.');
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {hostEquals: 'https://www.google.com/'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
   });
-
-let button = document.querySelector('#changeColor');
-function text(){
-  console.log("hello");
-}
-
-button.addEventListener("click",text)
